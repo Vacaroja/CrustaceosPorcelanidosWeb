32626d276965aa59a5fe3fe42125678c0b5171a7
CoalChillChicken <coalchillchicken@gmail.com>
2025-11-22 14:25:54 -0400

    quitado bug al cargar pestaña
    arreglado error al recargar pestaña (Redirección PRG) y comentado todo

--- pruebaFlask.py
+++ pruebaFlask.py
@@ -3,8 +3,10 @@ from constantes.preguntas import PREGUNTAS
 from modelo.prueba_tensorflow import modelo_cangrejo
 
 app = Flask(__name__)
+#secret key para la sesion y el guardado de la informacion
 app.secret_key = '1234566789'
 
+#ruta principal
 @app.route('/')
 def index():
     if 'pregunta_actual' in session:
@@ -12,44 +14,67 @@ def index():
         session['respuestas'] = []
     return render_template('index.html')
 
+#ruta de quiz para las preguntas
 @app.route('/quiz', methods=['GET', 'POST'])
 def quiz():
-    respuestas = []
+    #si es la primera pregunta crea la sesion e inicializa la preguntas
     if 'pregunta_actual' not in session:
         session['pregunta_actual'] = 0
         session['respuestas'] = []
         
-    
+    #toma la posicion de la pregunta actual
     indice = session['pregunta_actual']
 
     if request.method == 'POST':
+        #toma el valor de la seleccion
         seleccion = request.form.get('seleccionado')
+        
         try:
+            #lo intenta convertir en int para la comparacion
+            print(seleccion)
             recibido = int(seleccion)
+            
+            print(recibido)
             if recibido == 1:
+                #si presiona el primer boton el csv se guarda 1,0
                 session['respuestas'].append(0)
                 session['respuestas'].append(1)
             else:
+                #si presiona el segundo guarda un registro 0,1
                 session['respuestas'].append(1)
                 session['respuestas'].append(0)
-        except (ValueError, TypeError):
-            print("Error al convertir la selección a entero.")
-        if indice < len(PREGUNTAS):
             session['pregunta_actual'] = indice + 1
+        except(ValueError, TypeError):
+            print("error" + str(ValueError) + str(TypeError))
+            return redirect(url_for('quiz'))
+                
+                
+            
+        #condicional para ultima pregunta
+        if indice < len(PREGUNTAS):
+            return redirect(url_for('quiz'))
+        else:
+            session.pop('pregunta_actual')
+            return redirect(url_for('resultados'))
+    else:
+
+        if indice < len(PREGUNTAS):
+            
             actual = PREGUNTAS[indice]
             return render_template('quiz.html', pregunta=actual, num_pregunta=indice + 1, total_preguntas=len(PREGUNTAS))
 
         else:
             session.pop('pregunta_actual')
             return redirect(url_for('resultados'))
-    return render_template('quiz.html', pregunta=PREGUNTAS[indice], num_pregunta=indice + 1, total_preguntas=len(PREGUNTAS))
 
+#ruta para mostrar el resultado
 @app.route('/resultados')
 def resultados():
+    #primero toma las respuestas guardadas en la sesion en una lista
     respuestas_guardadas = session.get('respuestas', [])
+    #luego las para a una variable resultado y hace la llamada al modelo
     resultado = modelo_cangrejo(respuestas_guardadas)
-    # Aquí puedes calcular y mostrar los resultados usando session['respuestas']
-    return render_template('result.html', respuestas=respuestas_guardadas,resultado = resultado)
+    return render_template('result.html', resultado = resultado)
 
 
 
