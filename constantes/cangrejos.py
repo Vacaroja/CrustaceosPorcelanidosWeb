af00daf8d1482d1202ee8b991f2daf34d8ae31ce
Andrés Alfonzo <vr.alfonzo1@gmail.com>
2025-11-25 23:38:55 -0400

    constante para formateo de respuesta del modelo

---
+++ constantes/cangrejos.py
@@ -0,0 +1,12 @@
+
+CRAB_SPECIES = [
+    {'id': 1, 'nombre_cientifico': 'Neopisosoma_neglectum', 'nombre_display': 'Neopisosoma neglectum','image': 'images/crab/caparazon_rugoso.jpg'},
+    {'id': 2, 'nombre_cientifico': 'Neopisosoma_angustifrons', 'nombre_display': 'Neopisosoma angustifrons'},'image': 'images/crab/caparazon_rugoso.jpg',
+    {'id': 3, 'nombre_cientifico': 'Neopisosoma_orientale', 'nombre_display': 'Neopisosoma orientale','image': 'images/crab/caparazon_rugoso.jpg'},
+    {'id': 4, 'nombre_cientifico': 'Clastotoechus_nodosus', 'nombre_display': 'Clastotoechus nodosus','image': 'images/crab/caparazon_rugoso.jpg'},
+    {'id': 5, 'nombre_cientifico': 'Pachycheles_serratus', 'nombre_display': 'Pachycheles serratus','image': 'images/crab/caparazon_rugoso.jpg'},
+    {'id': 6, 'nombre_cientifico': 'Pachycheles_monilifer', 'nombre_display': 'Pachycheles monilifer','image': 'images/crab/caparazon_rugoso.jpg'},
+    {'id': 7, 'nombre_cientifico': 'Pachycheles_riseii', 'nombre_display': 'Pachycheles riseii','image': 'images/crab/caparazon_rugoso.jpg'},
+    {'id': 8, 'nombre_cientifico': 'Pachycheles_ackleianus', 'nombre_display': 'Pachycheles ackleianus','image': 'images/crab/caparazon_rugoso.jpg'},
+    {'id': 9, 'nombre_cientifico': 'Petrolisthes_tridentatus', 'nombre_display': 'Petrolisthes tridentatus','image': 'images/crab/caparazon_rugoso.jpg'},
+]
